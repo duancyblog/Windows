f12087499691b8e98542e26356c3ae7290f7936f
duancyblog <duancy2014@163.com>
2025-03-29 13:27:27 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Windows-Download",
+  description: "Allen2030's Web",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Downaload', link: '/markdown-examples' }
+    ],
+
+    base: "/Windows/",
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+
+    footer: {
+      copyright: "Copyright@ 2025 Allen2030 Microsoft是美国微软公司的商标 与本网站无任何从属关系",
+    },
+
+       // 设置搜索框的样式
+       search: {
+        provider: "local",
+        options: {
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              noResultsText: "无法找到相关结果",
+              resetButtonTitle: "清除查询条件",
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+              },
+            },
+          },
+        },
+      },
+
+  }
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Windows Download"
+  text: "Allen2030's Web Beta"
+  tagline: 一个国内Windows镜像下载站
+  image:
+    src: https://uhf.microsoft.com/images/microsoft/RE1Mu3b.png
+    alt: 背景
+  actions:
+    - theme: brand
+      text: Download
+      link: /markdown-examples
+    - theme: alt
+      text: 快速入门
+      link: /api-examples
+
+features:
+  - title: 好处
+    details: 无需再使用国外服务器下载镜像
+  - title: 下载方式
+    details: 可使用ed2k/官网服务器链接下载
+  - title: 缺点
+    details: 不是自己服务器，无法把控病毒。作者无法立刻更新
+---
+
